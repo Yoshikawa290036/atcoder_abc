eb0c67755a91e4a77b3a1eb66f9cf4b89fcb0abd
Yoshikawa290036 <290036hirokix@gmail.com>
2022-09-18 13:55:07 +0900

    config

--- config/atcoder.sh
+++ config/atcoder.sh
@@ -1,21 +1,21 @@
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   a.cpp
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   b.cpp
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   c.cpp
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   d.cpp
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   e.cpp
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   f.cpp
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   g.cpp
-cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   test.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   a.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   b.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   c.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   d.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   e.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   f.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   g.cpp
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/cppform.cpp   test.cpp
 
 
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  a.py
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  b.py
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  c.py
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  d.py
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  e.py
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  f.py
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  g.py
-cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  test.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  a.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  b.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  c.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  d.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  e.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  f.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  g.py
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/pyform.py  test.py
 
 mkdir -p .vscode
-cp /mnt/e/FREE_SPACE/atcoder/config/.vscode/c_cpp_properties.json .vscode/
+cp /mnt/c/cpp_workspace/atcoder/ABC/config/.vscode/c_cpp_properties.json .vscode/
