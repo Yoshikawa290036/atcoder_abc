0c890c6d0633750ff39561e989c43adfbedae79c
Yoshikawa290036 <290036hirokix@gmail.com>
2022-09-18 13:48:47 +0900

    [Add] config

---
+++ config/atcoder.sh
@@ -0,0 +1,21 @@
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   a.cpp
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   b.cpp
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   c.cpp
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   d.cpp
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   e.cpp
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   f.cpp
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   g.cpp
+cp /mnt/e/FREE_SPACE/atcoder/config/cppform.cpp   test.cpp
+
+
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  a.py
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  b.py
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  c.py
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  d.py
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  e.py
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  f.py
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  g.py
+cp /mnt/e/FREE_SPACE/atcoder/config/pyform.py  test.py
+
+mkdir -p .vscode
+cp /mnt/e/FREE_SPACE/atcoder/config/.vscode/c_cpp_properties.json .vscode/
