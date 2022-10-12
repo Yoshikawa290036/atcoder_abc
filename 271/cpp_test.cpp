bf337d17c170380944327e4fd1a7e5a88b1002ea
Yoshikawa290036 <290036hirokix@gmail.com>
2022-10-12 16:23:34 +0900

    271

--- 271/cpp_test.cpp
+++ 271/cpp_test.cpp
@@ -19,6 +19,64 @@ ll my_lower_bound(vector<T> &vec, T key);
 
 int main()
 {
+    ll N, sold = 0;
+    cin >> N;
+    map<ll, bool> have;
+    vector<ll> hlist;
+
+    ll a;
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> a;
+        if (a > N)
+        {
+            sold++;
+        }
+        else if (have[a])
+        {
+            sold++;
+        }
+        else
+        {
+            have[a] = true;
+            hlist.push_back(a);
+        }
+    }
+    ll used = 0;
+
+    for (size_t i = 1; i < N + 1; i++)
+    {
+        cout << sold << endl;
+        if (have[i])
+        {
+            used++;
+            continue;
+        }
+        if (sold >= 2)
+        {
+            sold -= 2;
+        }
+        else if (sold == 1)
+        {
+            sold--;
+            have[hlist[hlist.size()-1]] = false;
+            hlist.pop_back();
+        }
+        else
+        {
+            // sold == 0
+            cout << hlist.size() << endl;
+            if (hlist.size() < 2)
+            {
+                cout << i - 1 << endl;
+                return 0;
+            }
+            have[hlist[hlist.size() - 1]] = false;
+            have[hlist[hlist.size() - 2]] = false;
+            hlist.pop_back();
+            hlist.pop_back();
+        }
+    }
 
     return 0;
 }
